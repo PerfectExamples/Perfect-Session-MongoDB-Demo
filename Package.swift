304a62825bcda615c4337abee9bb0e9f6f4454f2
Jonathan Guthrie <jono@perfect.org>
2017-06-21 14:38:26 -0400

    update minor versions

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "Perfect-Session-MongoDB-Demo",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Session-MongoDB.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 1),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Session-MongoDB.git", majorVersion: 1),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
     ]
 )
