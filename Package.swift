59f259004bcd90c04fed63dd423e4f8a65aa0bbd
Jonathan Guthrie <jono@perfect.org>
2017-10-04 12:23:03 -0400

    update for perfect 3 / swift 4

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2017-01-12 22:02:02 +0000
+// Date: 2017-10-04 16:09:25 +0000
 import PackageDescription
 let package = Package(
-    name: "Perfect-Session-MongoDB-Demo",
-    targets: [],
-    dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Session-MongoDB.git", majorVersion: 1),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
-    ]
+	name: "Perfect-Session-MongoDB-Demo",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Session-MongoDB.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
+	]
 )
